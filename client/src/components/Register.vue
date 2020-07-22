8b150b93db8215bca63536305ac9dd6209b09a3d
Michaela Wagner <mickywagner@gmail.com>
2020-07-22 15:28:44 -0600

    Add basic html registration form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>Register</h1>
+
+    <input 
+      type="email"
+      name="email"
+      placeholder="email" />
+    <input 
+      type="password"
+      name="password"
+      placeholder="password" />
+    <br>
+    <button>Register</button>
   </div>
 </template>
 
