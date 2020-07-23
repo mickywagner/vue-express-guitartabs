adb0952e14911b2d24036a5ea8854dfc582b703c
Michaela Wagner <mickywagner@gmail.com>
2020-07-23 14:27:01 -0600

    Install joi and add validation for registration route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,9 @@
 const authenticationController = require('./controllers/authenticationController')
 
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
     app.post('/register', 
+        AuthenticationControllerPolicy.register,
         authenticationController.register
 )}
