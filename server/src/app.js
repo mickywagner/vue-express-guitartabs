2194b541f3ba186249302656ad0f2bfd504c59a6
Michaela Wagner <mickywagner@gmail.com>
2020-07-23 13:03:30 -0600

    Reconfigure app with separate routes file

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `hello ${req.body.email} user was registered. woot!`,
+require('./routes')
 
-    })
+sequelize.sync().then(() => {
+    app.listen(config.port || 8081, () => console.log(
+        `Server is running on port ${config.port}`
+    ))
 })
 
-app.listen(process.env.PORT || 8081, () => console.log(
-    'Server is running on port 8081'
-))
