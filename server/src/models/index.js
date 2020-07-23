d85ecd9a86f66cb18f4c847425c9ac91ebf8538f
Michaela Wagner <mickywagner@gmail.com>
2020-07-23 13:43:03 -0600

    Install sqlite3, create function to read files and load models into sequelize

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const config = require('../config/config')
-
 const db = {}
 
 const sequelize = new Sequelize(
@@ -12,4 +11,18 @@ const sequelize = new Sequelize(
     config.db.options
 )
 
-module.exports
+fs
+   .readdirSync(__dirname)
+   .filter((file) => 
+        file !== 'index.js'
+   )
+   .forEach((file) => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model
+   })
+
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
