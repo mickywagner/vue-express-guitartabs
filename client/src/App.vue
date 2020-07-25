c0ccd162763bce2cd45378a0f8b0fdd96618dd19
Michaela Wagner <mickywagner@gmail.com>
2020-07-24 19:34:35 -0600

    Add button and router link for register and home

--- client/src/App.vue
+++ client/src/App.vue
@@ -29,4 +29,11 @@ export default {
   color: #2c3e50;
 }
 
+a {
+  text-decoration: none;
+  color: white;
+}
+
+
+
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,8 +1,24 @@
 <template>
     <v-toolbar dark fixed class="cyan">
-        <v-toolbar-title class="mr-4">
-            TabTracker
-        </v-toolbar-title>
+        <router-link to="/">
+            <v-toolbar-title class="mr-4">
+                TabTracker
+            </v-toolbar-title>
+        </router-link>
+
+        <!-- <v-toolbar-items>
+            <v-btn flat dark>
+                Browse
+            </v-btn>
+        </v-toolbar-items> -->
+        <v-spacer></v-spacer>
+        <v-toolbar-itmes>
+                <router-link to="register">
+                    <v-btn text>
+                        Sign Up
+                    </v-btn>
+                </router-link>
+        </v-toolbar-itmes>
     </v-toolbar>
 </template>
 
@@ -15,6 +31,6 @@ export default {
 
 <style scoped>
 header {
-    max-height: 70px;
+    max-height: 50px;
 }
 </style>
