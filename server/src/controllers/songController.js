528edf60ee75c3edfc54e6448a924efa4f77b707
Michaela Wagner <mickywagner@gmail.com>
2020-08-05 14:24:20 -0600

    Backend songController search method

--- server/src/controllers/songController.js
+++ server/src/controllers/songController.js
@@ -1,56 +1,70 @@
-const {Song} = require('../models')
+const { Song, Sequelize } = require("../models");
+const Op = Sequelize.Op
 
 module.exports = {
-    async index (req, res) {
-        try {
-            const songs = await Song.findAll({
-                limit: 10
-            })
-            console.log('looking for songs')
-            res.send(songs)
-
-        } catch (error) {
-            console.log('in the catch')
-            res.status(500).send({
-                message: 'An error occured while fetching songs'
-            })
-        }
-    },
-
-    async post (req, res) {
-        try {
-            const song = await Song.create(req.body)
-            res.send(song)
-        } catch (error) {
-            res.status(500).send({
-                error: 'An error occurred while creating song'
-            })
-        }
-    },
-
-    async show (req, res) {
-        try {
-            const song = await Song.findOne({ where: {id: req.params.songId}})
-            res.send(song)
-        } catch (error) {
-            res.status(500).send({
-                message: 'Error occurred when fetching song'
-            })
-        }
-    },
-
-    async put (req, res) {
-        try {
-            const song = await Song.update(req.body, {
-                where: {
-                    id: req.params.songId
-                }
-            })
-            res.send(this.body)
-        } catch (err) {
-            res.status(500).send({
-                message: 'Error occured while trying to save song'
-            })
-        }
+  async index(req, res) {
+    try {
+      let songs = null
+      const search = req.query.search
+      
+      if (search) {
+        songs = await Song.findAll({
+            where: {
+                [Op.or]: [
+                    {title: search},
+                    {artist: search},
+                    {genre: search},
+                    {album: search}
+                ]
+            }
+        })
+      } else {
+        songs = await Song.findAll({
+          limit: 10,    
+        });
+      }
+      res.send(songs);
+    } catch (error) {
+      res.status(500).send({
+        message: "An error occured while fetching songs",
+      });
     }
-}
+  },
+
+  async post(req, res) {
+    try {
+      const song = await Song.create(req.body);
+      res.send(song);
+    } catch (error) {
+      res.status(500).send({
+        error: "An error occurred while creating song",
+      });
+    }
+  },
+
+  async show(req, res) {
+    try {
+      const song = await Song.findOne({ where: { id: req.params.songId } });
+      res.send(song);
+    } catch (error) {
+      res.status(500).send({
+        message: "Error occurred when fetching song",
+      });
+    }
+  },
+
+  async put(req, res) {
+    try {
+      const song = await Song.update(req.body, {
+        where: {
+          id: req.params.songId,
+        },
+      });
+      res.send(this.body);
+    } catch (err) {
+      res.status(500).send({
+        message: "Error occured while trying to save song",
+      });
+    }
+  },
+};
