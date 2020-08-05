ebbc69dd17d0718559b448e83fbb1e4b47158a36
Michaela Wagner <mickywagner@gmail.com>
2020-08-05 12:45:01 -0600

    Add search panel and watch method for search

---
+++ client/src/components/Songs/SearchPanel.vue
@@ -0,0 +1,44 @@
+<template>
+    <panel title="Search">
+        <form>
+           <v-text-field
+            label="Search by song title, artist, album, or genre"
+            v-model="search"
+           >
+           </v-text-field>
+        </form>
+    </panel>
+</template>
+
+<script>
+import Panel from '@/components/panel'
+
+export default {
+  name: 'SearchPanel',
+  components: {
+    Panel
+  },
+  data () {
+    return {
+      search: ''
+    }
+  },
+  watch: {
+    search (val) {
+      const route = {
+        name: 'songs'
+      }
+      if (this.search !== '') {
+        route.query = {
+          search: this.search
+        }
+        this.$router.push(route)
+      }  
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Songs/Songs.vue
+++ client/src/components/Songs/Songs.vue
@@ -1,18 +1,23 @@
 <template>
     <v-layout>
-    <v-flex xs8 offset-xs2>
-      <songs-panel></songs-panel>
+    <v-flex xs4 mr-2>
+      <search-panel />
+    </v-flex>
+    <v-flex xs8>
+      <songs-panel />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import SongsPanel from './SongsPanel'
+import SearchPanel from './SearchPanel'
 
 export default {
   name: 'Songs',
   components: {
-    SongsPanel
+    SongsPanel,
+    SearchPanel
   }
 }
 
