d8e6feea826c49265dfe6c2c8fa53f91220ffe54
Michaela Wagner <mickywagner@gmail.com>
2020-08-05 12:24:19 -0600

    Split Songs into new SongsPanel

---
+++ client/src/components/Songs/Songs.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-layout>
+    <v-flex xs8 offset-xs2>
+      <songs-panel></songs-panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongsPanel from './SongsPanel'
+
+export default {
+  name: 'Songs',
+  components: {
+    SongsPanel
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
