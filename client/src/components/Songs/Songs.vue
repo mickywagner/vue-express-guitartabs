8d9546ec8131d43f4118e6c238b535fae3e5772f
Michaela Wagner <mickywagner@gmail.com>
2020-08-13 11:51:32 -0600

    Create a Booksmarks component

--- client/src/components/Songs/Songs.vue
+++ client/src/components/Songs/Songs.vue
@@ -1,6 +1,7 @@
 <template>
     <v-layout>
     <v-flex xs4>
+      <songs-bookmarks />
     </v-flex>
     <v-flex xs8>
       <search-panel />
@@ -12,12 +13,14 @@
 <script>
 import SongsPanel from './SongsPanel'
 import SearchPanel from './SearchPanel'
+import SongsBookmarks from './SongsBookmarks'
 
 export default {
   name: 'Songs',
   components: {
     SongsPanel,
-    SearchPanel
+    SearchPanel,
+    SongsBookmarks
   }
 }
 
---
+++ client/src/components/Songs/SongsBookmarks.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-data-table>
+    </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'SongsBookmarks'
+}
+</script>
+
+<style scoped>
+
+</style>
